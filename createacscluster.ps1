df4f57f0163698926a54a43add419c859dc45ab3
Nick Ward <nickward@microsoft.com>
2017-03-16 15:38:30 +1100

    added kubernetes commands

--- createacscluster.ps1
+++ createacscluster.ps1
@@ -2,4 +2,22 @@
 az group create --name acsrg --location australiasoutheast
 
 # Create Azure Container Service cluster
-az acs create --name acs-cluster --resource-group acsrg --dns-prefix hect --generate-ssh-keys --orchestrator-type swarm
+az acs create --name acs-cluster --resource-group acsrg --dns-prefix hect --generate-ssh-keys --orchestrator-type kubernetes
+
+# Install kubernetes cli if needed
+# az acs kubernetes install-cli
+
+# Get master kubernetes cluster configuration file
+az acs kubernetes get-credentials --resource-group acsrg --name acs-cluster
+
+# Run a Container
+kubectl run simpleaspnetcoreapp --image hectagon/demos:simpleaspnetcoreapp
+
+# See container running
+kubectl get pods
+
+# Expose container on the Azure load balancer
+kubectl expose deployments simpleaspnetcoreapp --port=80 --target-port=5000 --type=LoadBalancer
+
+# verivy public ip address assigned to Service
+kubectl get svc
